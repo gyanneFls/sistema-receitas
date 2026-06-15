f846a2057d9c3b1f4cad3dbc31f652d954356c27
jpedronog <joaopedropaulinonogueira@gmail.com>
2026-06-15 08:50:38 -0300

    auau

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'teste')->name('home');
+Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('', 'welcome')->name('home');
+    Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
 require __DIR__.'/settings.php';

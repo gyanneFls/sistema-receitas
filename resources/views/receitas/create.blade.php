b686f16d56dafd6c5cb60ae455620035444ff565
Gustavo <ga6621165@gmail.com>
2026-06-16 12:44:14 -0300

    css create

--- resources/views/receitas/create.blade.php
+++ resources/views/receitas/create.blade.php
@@ -2,8 +2,12 @@
 
 @section('title', 'Sistema de Receitas')
 
+@section('css')
+    @vite('resources/css/create.css')
+@stop
+
 @section('content_header')
-    <h1>Sistema de Receitas</h1>
+    
 @stop
 
 @section('content')
@@ -11,33 +15,36 @@
         <div class="card-header">
             <h3 class="card-title">Adicione sua receita favorita, ou crie uma!</h3>
         </div>
-
+        <img src="{{ asset('vendor/adminlte/dist/img/ursologo.png') }}" class="logo">
         <div class="box">
-
         <form action="{{route('receitas.store')}}" method="POST" enctype="multipart/form-data">
             @csrf
              <h1>Cadastre sua receita</h1>
-            <label>Nome da receita</label>
+             <br>
+            <label>Nome da receita:
             <input type="text" name="nome" placeholder="Digite o nome da receita...">
-            <label>Descrição da receita</label>
+            </label>
+            <label>Descrição da receita:
             <input type="text" name="descricao" placeholder="Digite a descrição da receita...">
-
-            <label>Ingredientes</label>
+            </label>
+            <label>Ingredientes:
             <input type="text" name="ingredientes" placeholder="Digite os ingredientes utilizados...">
-
-            <label>Modo de preparo</label>
+            </label>
+            <label>Modo de preparo:
            <textarea id="modo_preparo" name="modo_preparo" placeholder="Digite o modo de preparo..."></textarea>
+           </label>
 
-            <label for="categoria_id">Categoria:</label>
+            <label for="categoria_id">Categoria:
             <select name="categoria_id" id="categoria_id" required>
                 <option value="">Selecione uma categoria</option>
                 @foreach($categorias as $categoria)
                     <option value="{{ $categoria->id }}">{{ $categoria->nome }}</option>
                 @endforeach
             </select>
-
-            <label for="imagem">Foto da Receita:</label>
+            </label>
+            <label for="imagem">Foto da Receita:
             <input type="file" name="imagem" id="imagem" accept="image/*" required>
+            </label>
 
             <button type="submit">Cadastrar</button>
             <a href="{{ route('receitas.index') }}" class="btn btn-secondary"> ← Voltar para lista </a>
